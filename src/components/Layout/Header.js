43b9dd4473cb9474dba55b7fd792b578333a03ab
James Serra <Jamesserra4@gmail.com>
2022-06-16 13:32:52 -0400

    Cart Reducer

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,11 +7,11 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>Meals</h1>
+        <h1>ReactMeals</h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
-        <img src={mealsImage} alt="a table of food" />
+        <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
     </Fragment>
   );
